b6c1b9d2ab799984fcd71c7c742bc00be275d60d
Chris Nadeau <31997838+cmnadeau@users.noreply.github.com>
2021-11-11 14:38:33 -0500

    Prototyped functions for edge detection
    Split up work regarding implementation of Canny edge detection algorithm

--- edge_detect.py
+++ edge_detect.py
@@ -1,1 +1,25 @@
+#libraries
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import part1.py #Gabby, smooth_image and gradient_map
+import part2.py #Chris, suppress_nommaxima, double_threshold
+#intake photo
+# greyscale
+def rgb2grey(rgbimg):
+  return np.dot(rgbimg[...,:3],[0.2989, 0.5870, 0.1140])
 
+def smooth_image(photo):  
+#Smooth image with Gaussian filter (using built-in fn
+
+
+def gradient_magnitude():
+ # Compute gradient magnitude and orientation w/ finite-diff approx. for partials
+def suppress_nonmaxima():
+# Apply nonmaxima suppression to the gradient magnitude
+
+
+def double_threshold():
+# Use double thresholding to detect and link edges
+# mask1 = input photo < theshold1
+# mask2 = input_photo < theshold2

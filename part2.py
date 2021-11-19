b106f53520880cb39c79f8d87a7178594b2bd6a2
cmnadeau <cmnadeau@uri.edu>
2021-11-18 16:45:50 -0800

    Corrected issue with naming of greyscaling input image, added simple test image

---
+++ part2.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL import Image, ImageFilter
+import imageio
+import scipy as sp
+import matplotlib as plt
+import cv2 as cv
+
+def suppress_nonmaxima():
+    pass
+
+def double_threshold():
+    pass
